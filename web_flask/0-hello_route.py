c6ba1db65f3e2cabceb6c7b06dbfb2759b9ca3d1
Michael-champions <immichaelchampionss@gmail.com>
2023-01-17 04:58:45 -0800

    a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+""" for the 0-hello_route """
+""" the route app must be listening on '0.0.0.0' """
+
+from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def get(self):
+
+        return {"HELLO HBNB!"}
+    if __name__ == '__main__'
+    app.run(host = '0.0.0.0', port = '5000')
