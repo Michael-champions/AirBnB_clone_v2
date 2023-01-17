3edf0561be54c992ae2b25a5b64771fc9e082911
Michael Attah <immichaelattah@gmail.com>
2023-01-17 05:17:55 -0800

    Add files via upload
     a script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""the `0-hello_route` module
+"""the `2-c_route` module
 starts a flask web application listening on `0.0.0.0:5000`
 """
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,6 +11,18 @@ app = Flask(__name__)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
